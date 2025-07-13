19d7dfd59a7c427da2a00b4f6f3603602cee095b
Øivind Thorrud <oeitho@gmail.com>
2025-07-14 00:38:26 +0200

    Add minimal git repository initialization script.

---
+++ src/command/initialize_repository/mod.rs
@@ -0,0 +1,50 @@
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::fs::File;
+#[cfg(target_os = "windows")]
+use windows::Win32::Storage::FileSystem;
+
+
+pub fn initialize_repository(path: Box<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    if path.exists() {
+        return Err(Box::from("Folder already exists."))
+    }
+    create_hidden_directory(path.clone())?;
+    create_directory(path.join("objects").into_boxed_path())?;
+    create_directory(path.join("refs").into_boxed_path())?;
+    create_head_file(path.clone())?;
+    Result::Ok(())
+}
+
+
+fn create_directory(path: Box<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    fs::create_dir(path)?;
+    Ok(())
+}
+
+fn create_hidden_directory(path: Box<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    fs::create_dir(path.clone())?;
+    #[cfg(target_os = "windows")]
+    make_directory_hidden(path)?;
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+fn make_directory_hidden(path: Box<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    unsafe {
+        use windows::{core::PCSTR, Win32::Storage::FileSystem::FILE_ATTRIBUTE_HIDDEN};
+        let directory_path: String = String::from(path.to_str().unwrap()) + "\0";
+
+        FileSystem::SetFileAttributesA(PCSTR::from_raw(directory_path.as_ptr()), FILE_ATTRIBUTE_HIDDEN)?;
+    }
+    Ok(())
+}
+
+fn create_head_file(path: Box<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut path: PathBuf = PathBuf::from(path.clone());
+    path.push("HEAD");
+    let mut file = File::create(path)?;
+    file.write_all(b"ref: refs/heads/master")?;
+    Result::Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod command;
+use crate::command::initialize_repository::initialize_repository;
+use std::{env, path::{Path, PathBuf}};
+
 fn main() {
-    println!("Hello, world!");
+    let result = initialize_repository(get_path());
+    match result {
+        Ok(()) => {
+            println!("Repository created");
+        }
+        Err(error) => {
+            panic!("{}", error);
+        }
+    };
+}
+
+fn get_path() -> Box<Path> {
+    let args: Vec<String> = env::args().collect();
+    let path = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        ".".to_string()
+    };
+    let path: PathBuf = [path.as_str(), ".git"].iter().collect();
+    path.into_boxed_path()
 }
