dfe91136491c18720ecfa8384505f1a330d5162a
Øivind Thorrud <oeitho@gmail.com>
2025-08-08 14:56:28 +0200

    Create empty info and pack directories inside of objects directory.

--- src/action/initialize_repository/mod.rs
+++ src/action/initialize_repository/mod.rs
@@ -4,22 +4,38 @@ use std::path::Path;
 
 pub fn initialize_repository(path: Box<Path>) -> Vec<Action> {
     let path = path.join(".git").into_boxed_path();
+    let mut actions = Vec::new();
+    actions.push(CreateDirectory {
+        path: path.clone(),
+        hidden: true,
+    });
+    actions.extend(objects_directory(path.clone()));
+    actions.push(CreateDirectory {
+        path: path.join("refs").into_boxed_path(),
+        hidden: true,
+    });
+    actions.push(CreateFile {
+        path: path.join("HEAD").into_boxed_path(),
+        content: b"ref: refs/heads/master".to_vec(),
+    });
+
+    actions
+}
+
+fn objects_directory(path: Box<Path>) -> Vec<Action> {
+    let path = path.join("objects").into_boxed_path();
     vec![
         CreateDirectory {
             path: path.clone(),
             hidden: true,
         },
         CreateDirectory {
-            path: path.join("objects").into_boxed_path(),
+            path: path.join("info").into_boxed_path(),
             hidden: true,
         },
         CreateDirectory {
-            path: path.join("refs").into_boxed_path(),
+            path: path.join("pack").into_boxed_path(),
             hidden: true,
-        },
-        CreateFile {
-            path: path.join("HEAD").into_boxed_path(),
-            content: b"ref: refs/heads/master".to_vec(),
         },
     ]
 }
